436e0488152cba15994e04d99a48f2bcf4199b24
Fabrizio Di Napoli <Hyunkel@Fabrizios-MacBook-Pro-2.local>
2015-04-03 00:21:54 +0200

    Add first working test WtiApiRequestTest

--- src/LinguaLeo/wti/WtiApiRequest.php
+++ src/LinguaLeo/wti/WtiApiRequest.php
@@ -10,162 +10,165 @@ use LinguaLeo\wti\Exception\WtiApiException;
  */
 class WtiApiRequest
 {
-	/**
-	 * @var
-	 */
-	private $resource;
+    /**
+     * @var
+     */
+    private $resource;
 
-	/**
-	 * Error.
-	 *
-	 * @var string
-	 */
-	private $error;
+    /**
+     * Error.
+     *
+     * @var string
+     */
+    private $error;
 
-	/**
-	 * Error number.
-	 *
-	 * @var integer
-	 */
-	private $errno;
+    /**
+     * Error number.
+     *
+     * @var integer
+     */
+    private $errno;
 
-	/**
-	 * Api result.
-	 *
-	 * @var mixed.
-	 */
-	private $result;
+    /**
+     * Api result.
+     *
+     * @var mixed.
+     */
+    private $result;
 
-	/**
-	 * Headers.
-	 *
-	 * @var mixed.
-	 */
-	private $headers;
+    /**
+     * Headers.
+     *
+     * @var mixed.
+     */
+    private $headers;
 
-	/**
-	 * Is request runned.
-	 *
-	 * @var boolean
-	 */
-	private $isRequestRunned = false;
+    /**
+     * Is request done.
+     *
+     * @var boolean
+     */
+    private $isRequestDone = false;
 
-	/**
-	 * Class constructor.
-	 *
-	 * @param $resource
-	 */
-	public function __construct($resource)
-	{
-		$this->resource = $resource;
-	}
+    /**
+     * Class constructor.
+     *
+     * @param $resource
+     */
+    public function __construct($resource)
+    {
+        $this->resource = $resource;
+    }
 
-	/**
-	 * Run.
-	 */
-	public function run()
-	{
-		$this->isRequestRunned = true;
-		$result = curl_exec($this->resource);
+    /**
+     * Run.
+     */
+    public function run()
+    {
+        $this->isRequestDone = true;
+        $result = curl_exec($this->resource);
 
-		$header_size = curl_getinfo($this->resource, CURLINFO_HEADER_SIZE);
-		$this->headers = $this->prepareHeaders(substr($result, 0, $header_size));
-		$this->result = substr($result, $header_size);
+        $header_size = curl_getinfo($this->resource, CURLINFO_HEADER_SIZE);
+        $this->headers = $this->prepareHeaders(substr($result, 0, $header_size));
+        $this->result = substr($result, $header_size);
 
-		if ($this->result === false) {
-			$this->error = curl_error($this->resource);
-			$this->errno = curl_errno($this->resource);
-		}
-	}
+        if ($this->result === false) {
+            $this->error = curl_error($this->resource);
+            $this->errno = curl_errno($this->resource);
+        }
+    }
 
-	/**
-	 * Prepare Headers.
-	 *
-	 * @param string $headersString
-	 * @return array
-	 */
-	private function prepareHeaders($headersString)
-	{
-		$headersArray = explode(PHP_EOL, $headersString);
-		// Remove first header, HTTP code
-		array_shift($headersArray);
+    /**
+     * Prepare Headers.
+     *
+     * @param string $headersString
+     * @return array
+     */
+    private function prepareHeaders($headersString)
+    {
+        $headersArray = explode(PHP_EOL, $headersString);
+        // Remove first header, HTTP code
+        array_shift($headersArray);
 
-		$headersAssoc = array();
+        $headersAssoc = array();
 
-		foreach ($headersArray as $header) {
-			if ($header === '') {
-				continue;
-			}
-			preg_match('~^([^:]*)\:(.*)$~', $header, $matches);
-			if (count($matches) == 3) {
-				$headersAssoc[$matches[1]] = trim($matches[2]);
-			}
-		}
+        foreach ($headersArray as $header) {
+            if ($header === '') {
+                continue;
+            }
+            preg_match('~^([^:]*)\:(.*)$~', $header, $matches);
+            if (count($matches) == 3) {
+                $headersAssoc[$matches[1]] = trim($matches[2]);
+            }
+        }
 
-		return $headersAssoc;
-	}
+        return $headersAssoc;
+    }
 
-	/**
-	 * Get error.
-	 *
-	 * @return mixed
-	 */
-	public function getError()
-	{
-		return $this->error;
-	}
+    /**
+     * Get error.
+     *
+     * @return mixed
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
 
-	/**
-	 * Get Error number.
-	 *
-	 * @return mixed
-	 */
-	public function getErrno()
-	{
-		return $this->errno;
-	}
+    /**
+     * Get Error number.
+     *
+     * @return mixed
+     */
+    public function getErrno()
+    {
+        return $this->errno;
+    }
 
-	/**
-	 * Get Raw result.
-	 *
-	 * @return mixed
-	 * @throws WtiApiException
-	 */
-	public function getRawResult()
-	{
-		if (!$this->isRequestRunned) {
-			throw new WtiApiException("Request must be performed before getting results.");
-		}
-		return $this->result;
-	}
+    /**
+     * Get Raw result.
+     *
+     * @return mixed
+     * @throws WtiApiException
+     */
+    public function getRawResult()
+    {
+        if (!$this->isRequestDone) {
+            throw new WtiApiException("Request must be performed before getting results.");
+        }
 
-	/**
-	 * Get result.
-	 *
-	 * @param boolean $assoc
-	 * @return mixed|null
-	 * @throws WtiApiException
-	 */
-	public function getResult($assoc = false)
-	{
-		if (!$this->isRequestRunned) {
-			throw new WtiApiException("Request must be performed before getting results.");
-		}
-		return $this->result ? json_decode($this->result, $assoc) : null;
-	}
+        return $this->result;
+    }
 
-	/**
-	 * Get Headers.
-	 *
-	 * @return mixed
-	 * @throws WtiApiException
-	 */
-	public function getHeaders()
-	{
-		if (!$this->isRequestRunned) {
-			throw new WtiApiException("Request must be performed before getting results.");
-		}
-		return $this->headers;
-	}
+    /**
+     * Get result.
+     *
+     * @param boolean $assoc
+     * @return mixed|null
+     * @throws WtiApiException
+     */
+    public function getResult($assoc = false)
+    {
+        if (!$this->isRequestDone) {
+            throw new WtiApiException("Request must be performed before getting results.");
+        }
+
+        return $this->result ? json_decode($this->result, $assoc) : null;
+    }
+
+    /**
+     * Get Headers.
+     *
+     * @return mixed
+     * @throws WtiApiException
+     */
+    public function getHeaders()
+    {
+        if (!$this->isRequestDone) {
+            throw new WtiApiException("Request must be performed before getting results.");
+        }
+
+        return $this->headers;
+    }
 
 } 
---
+++ tests/LinguaLeo/wti/WtiApiRequestTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace LinguaLeo\wti;
+
+/**
+ * Class WtiApiRequestTest
+ * @package LinguaLeo\wti
+ */
+class WtiApiRequestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Api fake URL.
+     *
+     * @var string
+     */
+    const API_FAKE_URL = 'https://webtranslateit.com/api/projects/fake';
+
+    /**
+     * Instance of WtiApiRequest.
+     *
+     * @var WtiApiRequest
+     */
+    private $wti_api_request;
+
+    /**
+     * Execute common code before each method.
+     */
+    public function setUp()
+    {
+        $resource = $this->getFakeCurlResource();
+        $this->wti_api_request = new WtiApiRequest($resource);
+    }
+
+    /**
+     * Get fake cURL resource.
+     *
+     * @param string $url Resource URL.
+     * @return Curl resource.
+     */
+    private function getFakeCurlResource($url = null)
+    {
+        $resource = curl_init();
+        curl_setopt($resource, CURLOPT_URL, $url);
+        curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($resource, CURLOPT_HEADER, 1);
+        curl_setopt($resource, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($resource, CURLOPT_POST, false);
+        curl_setopt($resource, CURLOPT_POSTFIELDS, array());
+        return $resource;
+    }
+
+    /**
+     * Test run with no result.
+     */
+    public function testRunWithNoResult()
+    {
+        $this->wti_api_request->run();
+        $result = $this->wti_api_request->getResult();
+        $this->assertNull($result);
+    }
+
+    /**
+     * Test run with error.
+     */
+    public function testRunWithError()
+    {
+        $this->wti_api_request->run();
+        $error = $this->wti_api_request->getError();
+        $errno = $this->wti_api_request->getErrno();
+        $this->assertEquals(3, $errno);
+        $this->assertEquals('<url> malformed', $error);
+    }
+
+    /**
+     * Test run with results as object.
+     */
+    public function testRunWithResultsAsObject()
+    {
+        $resource               = $this->getFakeCurlResource(static::API_FAKE_URL);
+        $this->wti_api_request  = new WtiApiRequest($resource);
+        $this->wti_api_request->run();
+        $result = $this->wti_api_request->getResult();
+        $this->assertTrue(is_object($result));
+        $this->assertFalse(is_array($result));
+    }
+
+    /**
+     * Test run with results as array.
+     */
+    public function testRunWithResultsAsArray()
+    {
+        $resource               = $this->getFakeCurlResource(static::API_FAKE_URL);
+        $this->wti_api_request  = new WtiApiRequest($resource);
+        $this->wti_api_request->run();
+        $result = $this->wti_api_request->getResult(true);
+        $this->assertFalse(is_object($result));
+        $this->assertTrue(is_array($result));
+    }
+
+    /**
+     * Test result has headers.
+     */
+    public function testResultHasHeaders()
+    {
+        $resource               = $this->getFakeCurlResource(static::API_FAKE_URL);
+        $this->wti_api_request  = new WtiApiRequest($resource);
+        $this->wti_api_request->run();
+        $headers = $this->wti_api_request->getHeaders();
+        $this->assertInternalType('array', $headers);
+        $this->assertNotEmpty($headers);
+        $this->assertArrayHasKey('Content-Type', $headers);
+    }
+
+    /**
+     * Test request result is raw.
+     */
+    public function testResultIsRaw()
+    {
+        $resource               = $this->getFakeCurlResource(static::API_FAKE_URL);
+        $this->wti_api_request  = new WtiApiRequest($resource);
+        $this->wti_api_request->run();
+        $raw_result = $this->wti_api_request->getRawResult();
+        $this->assertTrue(is_string($raw_result));
+    }
+
+    /**
+     * Test get result when request is not done.
+     *
+     * @throws Exception\WtiApiException
+     */
+    public function testGetResultWhenRequestIsNotDone()
+    {
+        $this->setExpectedException('LinguaLeo\wti\Exception\WtiApiException');
+        $this->wti_api_request->getResult();
+    }
+
+    /**
+     * Test get raw result when request is not done.
+     *
+     * @throws Exception\WtiApiException
+     */
+    public function testGetRawResultWhenRequestIsNotDone()
+    {
+        $this->setExpectedException('LinguaLeo\wti\Exception\WtiApiException');
+        $this->wti_api_request->getRawResult();
+    }
+
+    /**
+     * Test get headers when request is not done.
+     *
+     * @throws Exception\WtiApiException
+     */
+    public function testGetHeadersWhenRequestIsNotDone()
+    {
+        $this->setExpectedException('LinguaLeo\wti\Exception\WtiApiException');
+        $this->wti_api_request->getHeaders();
+    }
+}
